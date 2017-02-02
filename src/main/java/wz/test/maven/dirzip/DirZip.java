5cc9b36796af611c8df87d24eb864eb23f442e80
wuzhi <wuzhi2010@gmail.com>
2017-02-02 21:34:45 +0800

    初始化

---
+++ src/main/java/wz/test/maven/dirzip/DirZip.java
@@ -0,0 +1,13 @@
+package wz.test.maven.dirzip;
+
+import java.io.IOException;
+
+/**
+ * Created by admin on 2017/2/2.
+ */
+public class DirZip {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        System.out.println("this is main");
+    }
+}
---
+++ src/test/java/wz/test/maven/dirzip/DirZipTest.java
@@ -0,0 +1,13 @@
+package wz.test.maven.dirzip;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by admin on 2017/2/2.
+ */
+public class DirZipTest extends TestCase {
+    public void testMain() throws Exception {
+
+    }
+
+}
